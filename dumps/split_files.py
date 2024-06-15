658e033768360c6c30f17f83507c0717929f6f7d
staticpunch <134955821+staticpunch@users.noreply.github.com>
2024-06-15 12:38:54 +0700

    Create split_files.py

---
+++ dumps/split_files.py
@@ -0,0 +1,43 @@
+import glob
+import json
+from tqdm.notebook import tqdm
+import gzip
+madlad_dir = "data/madlad/*"
+out_dir = "data/dedup/"
+xfiles = glob.glob(madlad_dir)
+xfiles = sorted(xfiles)
+xfiles = [x for x in xfiles if ".gz" in x]
+
+buffer = []
+counter = 0
+THRESHOLD = 200_000
+for filepath in tqdm(xfiles[:]):
+    with gzip.open(filepath, "r") as file:
+        for line in file:
+            
+            ## add metadata for mapping
+            line = json.loads(line)
+            line.update({"metadata": {"source_file": filepath}})
+
+            ## write to file if the buffer reaching 200k lines.
+            if len(buffer) == THRESHOLD:
+                ## write file
+                idx = str(counter).zfill(4)
+                outfile = out_dir + f"madlad_part_{idx}.jsonl"
+                with open(outfile, "w") as out:
+                    for line in buffer:
+                        out.write(json.dumps(line, ensure_ascii=False) + "\n")
+                
+                ## next iterator.
+                buffer = []
+                counter += 1
+
+            ## append to buffer.
+            buffer.append(line)
+
+## write the remaining in the buffer to file
+idx = str(counter).zfill(4)
+outfile = out_dir + f"madlad_part_{idx}.jsonl"
+with open(outfile, "w") as out:
+    for line in buffer:
+        out.write(json.dumps(line, ensure_ascii=False) + "\n")
